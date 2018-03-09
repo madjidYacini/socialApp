932b99a03341c5c07add84e502a914b3067c59f9
madjid yacini <madjid.yacini@gmail.com>
2018-03-09 12:23:38 +0100

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ hi: 'hello world' });
+  res.json({ hi: 'hello efreitech' });
 });
 
 module.exports = router;
